b2bd8714ff61a15b05e224514bace38ed2b05f05
Brandon Cash <brandon@40digits.com>
2015-01-13 20:31:05 -0600

    So begins threelines

---
+++ assets/scripts/demo.js
@@ -0,0 +1,16 @@
+//
+// This demo provides buttons that change out the threelines icon
+//
+
+$(document).ready(function () {
+	var $threelinesDemo = $('#threelines-demo'),
+		$threelines = $threelinesDemo.find('.threelines'),
+		$frameButtons = $threelinesDemo.find('button');
+
+	$frameButtons.click(function (event) {
+		var newClass = $(this).data('frame');
+
+		$threelines
+			.attr('data-frame', newClass);
+	});
+});
