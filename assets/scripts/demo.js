dd1ecd4f7f201b9bca159452b4681f71e92ca250
Brandon Cash <brandon@40digits.com>
2015-01-13 22:42:42 -0600

    Add frame name label

--- assets/scripts/demo.js
+++ assets/scripts/demo.js
@@ -5,12 +5,15 @@
 $(document).ready(function () {
 	var $threelinesDemo = $('#threelines-demo'),
 		$threelines = $threelinesDemo.find('.examples .threelines'),
-		$frameButtons = $threelinesDemo.find('button');
+		$frameButtons = $threelinesDemo.find('button'),
+		$currentFrameName = $('#current-frame-name');
 
 	$frameButtons.click(function (event) {
 		var newClass = $(this).data('frame');
 
 		$threelines
 			.attr('data-frame', newClass);
+
+		$currentFrameName.text(newClass);
 	});
 });
